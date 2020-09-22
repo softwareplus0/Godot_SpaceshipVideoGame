0999c1dd6d3206bc32fd3bde2a5414a17181bcb8
alex <aramo173@fiu.edu>
2020-09-22 01:47:56 -0400

    added projectile delay

--- Player.gd
+++ Player.gd
@@ -8,23 +8,40 @@ var speed = 200
 
 onready var sprite : Sprite = get_node("sprite_of_player")
 
+var start_time = 0
+var end_time = 0
+var first_projectile = false;
+
 func _physics_process(delta):
 	velocity.x = 0
 	velocity.y = 0
 	sprite.rotation_degrees = 0
+
+
 	
 	if Input.is_action_pressed("spawn_projectile"):
-		var new_projectile = projectile_asset.instance()
 		
-		var newPos = Vector2(position.x, position.y) # This here sets pos based on the previous X value.
+		start_time = OS.get_ticks_msec()
 		
-		##########################
+		print("START_TIME: " + str(start_time))
+		print("END_TIME: " + str(end_time))
 		
-		new_projectile.position.x = newPos.x
-		new_projectile.position.y = newPos.y
-		
-		get_tree().get_root().add_child(new_projectile)
-	
+		if(abs(end_time - start_time) > 100 or first_projectile == false):
+			print("testing")
+			first_projectile = true
+			var new_projectile = projectile_asset.instance()
+			
+			var newPos = Vector2(position.x + (get_node("sprite_of_player").texture.get_size().x / 2), 
+			position.y + (get_node("sprite_of_player").texture.get_size().y / 2)) # This here sets pos based on the previous X value.
+			
+			##########################
+			
+			new_projectile.position.x = newPos.x
+			new_projectile.position.y = newPos.y
+			
+			get_tree().get_root().add_child(new_projectile)
+			
+			end_time = OS.get_ticks_msec()
 	
 	
 	
