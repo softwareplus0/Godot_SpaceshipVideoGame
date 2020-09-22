a7eb1c5faca462105cd4721fdda195e6ff7f21c6
alex <aramo173@fiu.edu>
2020-09-22 01:08:00 -0400

    added lasers

--- Player.gd
+++ Player.gd
@@ -1,5 +1,7 @@
 extends KinematicBody2D
 
+onready var projectile_asset = preload("res://Projectile.tscn")
+
 var velocity : Vector2 = Vector2()
 
 var speed = 200
@@ -10,6 +12,21 @@ func _physics_process(delta):
 	velocity.x = 0
 	velocity.y = 0
 	sprite.rotation_degrees = 0
+	
+	if Input.is_action_pressed("spawn_projectile"):
+		var new_projectile = projectile_asset.instance()
+		
+		var newPos = Vector2(position.x, position.y) # This here sets pos based on the previous X value.
+		
+		##########################
+		
+		new_projectile.position.x = newPos.x
+		new_projectile.position.y = newPos.y
+		
+		get_tree().get_root().add_child(new_projectile)
+	
+	
+	
 	
 	if Input.is_action_pressed("move_up"):
 		velocity.y = -speed
