c13220426fa580904a60d7afeec9db0f5a5d89f0
alex <aramo173@fiu.edu>
2020-09-22 00:50:26 -0400

    before adding functionality

--- MainScene.gd
+++ MainScene.gd
@@ -7,10 +7,12 @@ onready var screenSize = get_viewport().get_visible_rect().size
 # var a = 2
 # var b = "text"
 
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	while(1) :
-		var flagPrev = Vector2() # Give here a good starting value.
+		
 		var flag = flagPacked.instance()
 		
 		var newPos = Vector2(screenSize.x, randi() % int(screenSize.y)) # This here sets pos based on the previous X value.
@@ -19,7 +21,9 @@ func _ready():
 		
 		flag.position.x = newPos.x
 		flag.position.y = newPos.y
-		flagPrev = newPos #Update the previous in seach flag spawn.
+		
 		
 		get_tree().get_root().add_child(flag)
+		
+		
 		yield(get_tree().create_timer(1), "timeout")
